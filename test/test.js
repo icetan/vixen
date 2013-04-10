19c9006d499558e3864a7dd07639a1e6e3deb871
Christopher Fredén <christopher.freden@kartena.se>
2013-04-10 22:38:47 +0200

    Moved t.plan back to top of each test, tape doesn't run otherwise

--- test/test.js
+++ test/test.js
@@ -6,6 +6,7 @@ function getBody(window) {
 
 module.exports = function(test, jsdom) {
   test('should reflect view model changes in div', function(t) {
+    t.plan(1);
     jsdom.env(
       '<html><body><div id="test">{{test}}</div></body></html>', [],
       function(err, window) {
@@ -15,10 +16,10 @@ module.exports = function(test, jsdom) {
         t.equal(div.textContent, 'lol');
       }
     );
-    t.plan(1);
   });
 
   test('should reflect view model changes in attribute', function(t) {
+    t.plan(1);
     jsdom.env(
       '<html><body><input type="text" id="test" value="{{test}}"></body></html>', [],
       function(err, window) {
@@ -28,10 +29,10 @@ module.exports = function(test, jsdom) {
         t.equal(input.value, 'lol');
       }
     );
-    t.plan(1);
   });
 
   test('should render nested values in view model', function(t) {
+    t.plan(1);
     jsdom.env(
       '<html><body><div id="test">{{test.a}} and {{test.b}}</div></body></html>', [],
       function(err, window) {
@@ -41,10 +42,10 @@ module.exports = function(test, jsdom) {
         t.equal(div.textContent, 'lol and rofl');
       }
     );
-    t.plan(1);
   });
 
   test('should iterate list values in view model', function(t) {
+    t.plan(2);
     jsdom.env(
       '<html><body><for value="test" in="tests"><i>{{test}}</i></for></body></html>', [],
       function(err, window) {
@@ -56,10 +57,10 @@ module.exports = function(test, jsdom) {
         t.equal(body.textContent, 'lolroflomg');
       }
     );
-    t.plan(2);
   });
 
   test('should iterate object values in view model', function(t) {
+    t.plan(2);
     jsdom.env(
       '<html><body><for value="test" key="i" in="tests"><b>{{i}}</b>:<i>{{test}},</i></for></body></html>', [],
       function(err, window) {
@@ -70,10 +71,10 @@ module.exports = function(test, jsdom) {
         t.equal(body.textContent, 'a:lol,b:rofl,c:omg,');
       }
     );
-    t.plan(2);
   });
 
   test('should iterate values in nested list', function(t) {
+    t.plan(2);
     jsdom.env(
       '<html><body><for value="test" key="i" in="p.tests"><i>{{i}}:{{test}},</i></for></body></html>', [],
       function(err, window) {
@@ -84,10 +85,10 @@ module.exports = function(test, jsdom) {
         t.equal(body.textContent, '0:lol,1:rofl,2:omg,');
       }
     );
-    t.plan(2);
   });
 
   test('should execute for-each function specified by data-each attribute and filter', function(t) {
+    t.plan(4*3 + 2);
     jsdom.env(
       '<html><body><for each="foeach" value="test" in="tests"><i>{{test}}</i></for></body></html>', [],
       function(err, window) {
@@ -108,10 +109,10 @@ module.exports = function(test, jsdom) {
         t.equal(body.textContent, 'lolomg');
       }
     );
-    t.plan(4*3 + 2);
   });
 
   test('should attach each element before for-each function and remove after if filtered', function(t) {
+    t.plan(1*3 + 2);
     jsdom.env(
       '<html><body><for each="foeach" value="test" key="i" in="tests">{{i}}: <i>{{test}}</i></for></body></html>', [],
       function(err, window) {
@@ -128,10 +129,10 @@ module.exports = function(test, jsdom) {
         t.equal(body.textContent, '0: lol2: omg');
       }
     );
-    t.plan(1*3 + 2);
   });
 
   test('should attach each element before for-each function and remove after if filtered with on element iterator', function(t) {
+    t.plan(1*3 + 2);
     jsdom.env(
       '<html><body><div id="test" data-each="foeach" data-value="test" data-key="i" data-in="tests">{{i}}: <i>{{test}}</i></div></body></html>', [],
       function(err, window) {
@@ -148,10 +149,10 @@ module.exports = function(test, jsdom) {
         t.equal(div.textContent, '0: lol2: omg');
       }
     );
-    t.plan(1*3 + 2);
   });
 
   test('should iterate over new style iterator and re-iterate without traces', function(t) {
+    t.plan(2);
     jsdom.env(
       '<html><body>before<for value="val" key="i" in="stuff">{{i}}:<i>{{val}}</i>,</for>after</body></html>', [],
       function(err, window) {
@@ -164,10 +165,10 @@ module.exports = function(test, jsdom) {
         t.equal(body.textContent, 'beforez:8,x:yo,y:true,after');
       }
     );
-    t.plan(2);
   });
 
   test('should create elements inside <select>', function(t) {
+    t.plan(3);
     jsdom.env(
       '<html><body><select id="test" value="{{sel}}" data-value="val" data-key="i" data-in="stuff"><option value="{{i}}">{{val}}</select></body></html>', [],
       function(err, window) {
@@ -189,10 +190,10 @@ module.exports = function(test, jsdom) {
         t.notEqual(viewModel.sel, undefined);
       }
     );
-    t.plan(3);
   });
 
   test('should keep each iterated item in it\'s render scope so handlers are mapped correctly', function(t) {
+    t.plan(3);
     jsdom.env(
       '<html><body><for value="thing" key="i" in="stuff">{{i}}:<i id="thing-{{i}}" onclick="{{thing.on}}">{{thing.id}}</i>,</for></body></html>', [],
       function(err, window) {
@@ -226,10 +227,10 @@ module.exports = function(test, jsdom) {
         });
       }
     );
-    t.plan(3);
   });
 
   test('should fire event handlers', function(t) {
+    t.plan(3);
     jsdom.env(
       '<html><body><div id="test" onclick="{{handler}} {{ handler.extra }}"></div></body></html>', [],
       function(err, window) {
@@ -249,10 +250,10 @@ module.exports = function(test, jsdom) {
         });
       }
     );
-    t.plan(3);
   });
 
   test('should fire event handlers and give correct value', function(t) {
+    t.plan(3);
     jsdom.env(
       '<html><body><input type=text id="test" onchange="{{handler}}"></body></html>', [],
       function(err, window) {
@@ -271,10 +272,10 @@ module.exports = function(test, jsdom) {
         });
       }
     );
-    t.plan(3);
   });
 
   test('should chain values through functions', function(t) {
+    t.plan(2);
     jsdom.env(
       '<html><body><div id="test">{{value | format}}kr</div></body></html>', [],
       function(err, window) {
@@ -290,10 +291,10 @@ module.exports = function(test, jsdom) {
         t.equal(div.textContent, '10kr');
       }
     );
-    t.plan(2);
   });
 
   test('should remove all curlies', function(t) {
+    t.plan(2);
     jsdom.env(
       '<html><body><div id="test" class="error {{status}}"><b>Error</b>: {{message}}</div></body></html>', [],
       function(err, window) {
@@ -303,10 +304,10 @@ module.exports = function(test, jsdom) {
         t.equal(div.className, 'error ');
       }
     );
-    t.plan(2);
   });
 
   test('should remove event attributes', function(t) {
+    t.plan(1);
     jsdom.env(
       '<html><body><button id="test" onclick="{{click}}">Click me!</button></body></html>', [],
       function(err, window) {
@@ -315,10 +316,10 @@ module.exports = function(test, jsdom) {
         t.equal(button.getAttribute('onclick'), null);
       }
     );
-    t.plan(1);
   });
 
   test('should not traverse child elements with data-subview attribute', function(t) {
+    t.plan(1);
     jsdom.env(
       '<html><body><div id="test" data-subview>{{lol}}</div></body></html>', [],
       function(err, window) {
@@ -328,10 +329,10 @@ module.exports = function(test, jsdom) {
         t.equal(div.textContent, '{{lol}}');
       }
     );
-    t.plan(1);
   });
 
   test('should traverse root element even though it has a data-subview attribute', function(t) {
+    t.plan(1);
     jsdom.env(
       '<html><body data-subview><div id="test">{{lol}}</div></body></html>', [],
       function(err, window) {
@@ -341,10 +342,10 @@ module.exports = function(test, jsdom) {
         t.equal(div.textContent, 'meh');
       }
     );
-    t.plan(1);
   });
 
   test('should handle bi-directional properties for attributes', function(t) {
+    t.plan(3);
     jsdom.env(
       '<html><body><input id="test" value="{{val}}"></body></html>', [],
       function(err, window) {
@@ -358,10 +359,10 @@ module.exports = function(test, jsdom) {
         t.equal(input.value, 'tripledouble');
       }
     );
-    t.plan(3);
   });
 
   test('should handle bi-directional attributes for boolean attributes', function(t) {
+    t.plan(8);
     jsdom.env(
       '<html><body><input id="test" type="checkbox" checked="{{checked}}"></body></html>', [],
       function(err, window) {
@@ -380,10 +381,10 @@ module.exports = function(test, jsdom) {
         t.equal(viewModel.checked, false);
       }
     );
-    t.plan(8);
   });
 
   test('should only add bi-directional bindings for non-templated attributes', function(t) {
+    t.plan(6);
     jsdom.env(
       '<html><body><input id="test" type="text" value="{{text}}{{moreText}}" class="large {{classes}}"></body></html>', [],
       function(err, window) {
@@ -399,10 +400,10 @@ module.exports = function(test, jsdom) {
         t.equal(viewModel.classes, 'valid');
       }
     );
-    t.plan(6);
   });
 
   test('should not ignore falsy values in string templates', function(t) {
+    t.plan(4);
     jsdom.env(
       '<html><body><a id="link" href="http://bla/{{id}}">{{text}} {{id}}</a></body></html>', [],
       function(err, window) {
@@ -416,10 +417,10 @@ module.exports = function(test, jsdom) {
         t.equal(viewModel.text, false);
       }
     );
-    t.plan(4);
   });
 
   test('should be possible to pass values on construction of view model', function(t) {
+    t.plan(5);
     jsdom.env(
       '<html><body><a id="link" href="http://bla/{{id}}">{{text}} {{id}}</a><for value="item" in="items">{{item}}</for></body></html>', [],
       function(err, window) {
@@ -437,10 +438,10 @@ module.exports = function(test, jsdom) {
         t.equal(body.textContent, 'apa lol123');
       }
     );
-    t.plan(5);
   });
 
   test('should not call chaining functions with empty values or unnecessarily in iterators', function(t) {
+    t.plan(2 + 3);
     jsdom.env(
       '<html><body><for value="thing" key="i" in="stuff"><div class="{{i | alt}}">{{thing}}</div></for></body></html>', [],
       function(err, window) {
@@ -457,10 +458,10 @@ module.exports = function(test, jsdom) {
         t.equal(body.children[1].className, 'odd');
       }
     );
-    t.plan(2 + 3);
   });
 
   test('object used in second argument is same as proxy object', function(t) {
+    t.plan(3);
     jsdom.env(
       '<html><body>{{a}}</body></html>', [],
       function(err, window) {
@@ -473,6 +474,5 @@ module.exports = function(test, jsdom) {
         t.equal(body.textContent, '2');
       }
     );
-    t.plan(3);
   });
 };
